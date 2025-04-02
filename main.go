1930fded8b9ba24451aeae5c7bcfb9412728df6c
HarkHorning <hjhorning@gmai.com>
2025-04-02 00:23:25 -0400

    following tutorial to learn basics

--- main.go
+++ main.go
@@ -1,20 +1,52 @@
 package main
 
 import (
+	"image"
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-type Game struct{}
+type Game struct {
+	ChaSprite *ebiten.Image
+	X, Y      float64
+}
 
 func (g *Game) Update() error {
+
+	if ebiten.IsKeyPressed((ebiten.KeyRight)) {
+		g.X += 2
+	}
+	if ebiten.IsKeyPressed((ebiten.KeyLeft)) {
+		g.X -= 2
+	}
+	if ebiten.IsKeyPressed((ebiten.KeyUp)) {
+		g.Y -= 2
+	}
+	if ebiten.IsKeyPressed((ebiten.KeyDown)) {
+		g.Y += 2
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+
+	screen.Fill(color.RGBA{110, 120, 255, 1})
+
+	// ebitenutil.DebugPrint(screen, "Good, evnin!")
+
+	opts := ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(g.X, g.Y)
+
+	screen.DrawImage(
+		g.ChaSprite.SubImage(
+			image.Rect(0, 0, 64, 64),
+		).(*ebiten.Image),
+		&opts,
+	)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -24,7 +56,12 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Good, evnin!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	Chara, _, err := ebitenutil.NewImageFromFile("models/img/wizard_sheet.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := ebiten.RunGame(&Game{ChaSprite: Chara, X: 60, Y: 0}); err != nil {
 		log.Fatal(err)
 	}
 }
